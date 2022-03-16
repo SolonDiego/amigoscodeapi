870e53b92d5f8343f3285eda704de4dc04ccb5b8
Solon Diego Garcia Moreira <sdiegogm@gmail.com>
2022-03-16 20:34:00 -0300

    update

--- src/main/java/br/com/solondiego/amigoscodeapi/entities/Student.java
+++ src/main/java/br/com/solondiego/amigoscodeapi/entities/Student.java
@@ -4,12 +4,28 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+
+@Entity
+@Table(name = "student")
 public class Student {
+
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    @Column(name = "id_student")
     private long id;
     private String name;
     private String email;
